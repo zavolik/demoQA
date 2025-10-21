ff1346243224036e3d38f6aec094b6f732df0c78
Olga Spiridonova <spiri.o.s@yandex.ru>
2025-10-21 21:15:08 +0300

    /

--- pages/text_box.py
+++ pages/text_box.py
@@ -2,9 +2,18 @@ from pages.base_page import BasePage
 from components.components import WebElement
 
 class TextBox(BasePage):
-
+    driver = ""
     def __init__(self, driver):
         self.base_url = "https://demoqa.com/text-box"
+        self.driver = driver
         super().__init__(driver, self.base_url)
 
-        self.full_name = WebElement(driver, "#userName") # поле для ввода имени пользователя
+        self.full_name = WebElement(driver, '#userName') # поле для ввода имени пользователя
+        self.current_address = WebElement(driver,'#currentAddress')
+        self.submit_button = WebElement(driver, '#submit')
+
+    def get_name(self):
+        return WebElement(self.driver, '#name').get_text()
+
+    def get_address(self):
+        return WebElement(self.driver, '#currentAddress.mb-1').get_text()
---
+++ tests/tests_hw/test_text_box.py
@@ -0,0 +1,18 @@
+from pages.text_box import TextBox
+# 1
+def test_text_box(browser):
+    text_box_page = TextBox(browser)
+    text_box_page.visit()
+
+    # просьба вводить данные через переменные
+    name = 'Мария'
+    result_name = 'Name:'+name
+    address = 'Москва'
+    result_address = 'Current Address :'+address
+
+    text_box_page.full_name.send_keys(name)
+    text_box_page.current_address.send_keys(address)
+    text_box_page.submit_button.click_force()
+
+    assert text_box_page.get_name() == result_name
+    assert text_box_page.get_address() == result_address
